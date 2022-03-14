6215f4ffe57180fd80421e4eb0106b44f5541d4c
Benjamin Aguilar <bencode2800@gmail.com>
2022-03-14 18:27:50 -0400

    finished Js DOM exercise

---
+++ js19.js
@@ -0,0 +1,25 @@
+// check for bugs and tell me what kind of data type it is
+let cars = ["Saab", "Volvo", "BMW"]; //array
+let a = "words" //string
+let age = 15 //integer
+const car = {type:"Fiat", model:500, color:"white"}; //object
+(10 > 9) //boolean
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };  //explain this object function as best as you can
+  //it has two values and function returns the frst and second
+  // code an example of a method using a string or number data type
+  let b=2.356
+  let c=b.toFixed
+  // code an example of a method with an object or array data type
+  const waterbottle = {water:"yes", label:"yes", size:"meduim" }
+  // write an example of a constructor
+  constructor(name,homeTown,power,health,specialAttack,level)
+  // write practical code that can have visible changes on an html document(example setIntrevals and .replace)
+  let text = document.getElementById("demo").innerHTML; // creating a var using the method getElementById the ID Demo into a variable
+document.getElementById("demo").innerHTML = text.replace("Microsoft" , "Spotify"); // We used the element getElementById we used the method . replace to switch microsoft with spotify
+  // lastly update your github repo by using the Git add . , git commit -m"finshed todays assignment" and lastly git push.
